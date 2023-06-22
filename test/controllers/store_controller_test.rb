8c8d3196ac9e0beea24cf4b0bdc03cfbb78b784d
Boobalan AP <boobalan@zinclearninglabs.com>
2023-06-22 13:51:41 +0530

    feat: add visit count to store index.

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -10,4 +10,19 @@ class StoreControllerTest < ActionDispatch::IntegrationTest
     assert_select '.price', /\$[,\d]+\.\d\d/
   end
 
+  test 'should show 1 visit when the user loads index first time' do
+    get store_index_url
+    assert_response :success
+
+    assert_select '.visits', '1 visit'
+  end
+
+  test 'should show 2 visits when the user loads index second time' do
+    get store_index_url
+    get store_index_url
+    assert_response :success
+
+    assert_select '.visits', '2 visits'
+  end
+
 end
