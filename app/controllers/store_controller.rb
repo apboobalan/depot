6b4d28ea3356daac0da05ff84f78429d23b03022
Boobalan AP <boobalan@zinclearninglabs.com>
2023-06-23 23:11:09 +0530

    feat: add item to cart using ajax.

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,8 @@
 class StoreController < ApplicationController
+  include CurrentCart
   include VisitCounter
+
+  before_action :set_cart
   before_action :increment_visits, only: [:index]
   def index
     @products = Product.order(:title)
--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -44,6 +44,17 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
     assert_select 'td', 'Programming Ruby 1.9'
   end
 
+  test "should create line_item via ajax" do
+    assert_difference('LineItem.count') do
+      post line_items_url, params: {product_id: products(:ruby).id}, xhr: true
+    end
+
+    puts @response.body
+
+    assert_response :success
+    assert_match /<tr class=\\"line-item-highlight/, @response.body
+  end
+
   test "should return appropriate quantity when adding unique and duplicate products" do
     2.times {post line_items_url, params: { product_id: products(:ruby).id }}
     post line_items_url, params: { product_id: products(:one).id }
