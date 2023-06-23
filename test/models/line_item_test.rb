ce98b81ba5a595feb4b8812c5f876156c4ec5f8a
Boobalan AP <boobalan@zinclearninglabs.com>
2023-06-23 11:51:45 +0530

    fix: calculate total price of line item using the already set price of product.

--- test/models/line_item_test.rb
+++ test/models/line_item_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class LineItemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @product = products(:ruby)
+    @cart = carts(:one)
+  end
+  test "total price of line item is calculated using set price even if product price increases" do
+    old_price = @product.price
+    2.times do
+      line_item = @cart.add_product(@product)
+      line_item.save!
+    end
+
+    @product.price += 1
+    @product.save!
+
+    assert @product.price != old_price
+    assert LineItem.last.total_price == old_price * 2
+
+  end
 end
